3c87dd21736874cc1c0ed5dab663c2a1383d2468
Nathy1208 <smaniotonathaly0@gmail.com>
2025-02-06 11:28:03 -0300

    06.02.2025

---
+++ IntroducaoJS/Scripts01Operacaoes.js
@@ -0,0 +1,33 @@
+let a = 10
+let b = 3
+
+console.log("soma:");
+console.log(a+b); //13
+console.log("subtração:");
+console.log(a-b); //7
+console.log("Multiplicação:");
+console.log(a*b); //30
+console.log("Divisão:");
+console.log(a/b); //3.33
+console.log("resto da divisão");
+console.log(a%b); //1
+
+//operações relacionais
+// (> , <=, <, <=, ==, ===)
+console.log("Operações Relacionais")
+console.log(5>10)//false
+console.log(10 == "10"); //true
+console.log(10 === "10"); //false
+
+//Operações Lógicos
+let nota1 = 5;
+let nota2 = 8;
+
+//our (||)
+console.log(nota1>=7 || nota2>7); //true
+
+// and (&&)
+console.log(nota1>=7 && nota2 >=7);//false
+
+// not (!)
+console.log(!true); //false
