a7512de847c3009e88a31a04f71e65bbc8529519
Nathy1208 <smaniotonathaly0@gmail.com>
2025-08-08 13:34:39 -0300

    08/08/2025

--- FrameWorks/Angular/projeto-auth/src/app/services/auth.service.ts
+++ FrameWorks/Angular/projeto-auth/src/app/services/auth.service.ts
@@ -14,20 +14,25 @@ export class AuthService {
   constructor(private router: Router, private http: HttpClient) { }
 
   // métodos
-
+  //cadastrar o usuário no sistema
   registrar(usuario: any): Observable<any>{
+    //verificar se usuário no sistema
     return this.http.get<any[]>(`${this.apiUrl}?email=${usuario.email}`).pipe(
       map(usuarios => {
+        //se usuário já existe
         if (usuarios.length>0){
+          //lanço um erro para sistema
           throw new Error('Usuário já cadastrado');
         }
         return usuario
       }),
+      //caso o usuário não exista
       switchMap(novoUsuario =>
         this.http.post(this.apiUrl,novoUsuario).pipe(
           tap(()=> alert('Registro realizado com sucesso'))
         )
       ),
+      //pegar erros de conexão
       catchError(err => {
         alert(`Erro: ${err.message}`);
         throw err;
@@ -35,37 +40,48 @@ export class AuthService {
     );
   }
 
+  //método para logar usuários já registrados
   login(credenciais: any): Observable<boolean> {
+    //passar para o banco uma busca com email e senha
     return this.http
       .get<any[]>(
         `${this.apiUrl}?email=${credenciais.email}&senha=${credenciais.senha}`
       )
       .pipe(
+        //usuário existe com email e senha cadastrados
         map((usuarios) => {
+          //não encontrado 
           if (usuarios.length === 0) {
             return false;
           } else {
+            //usuário e sua chave de autenticação => localstorage 
             const usuario = usuarios[0];
             localStorage.setItem(this.CHAVE_AUTH, JSON.stringify(usuario));
-            return true;
+            return true; //deu certo, continua
           }
         })
       );
   }
   
-
+//deslogar o user
   logout() {
+    //llocal storage 
     localStorage.removeItem(this.CHAVE_AUTH);
+    //redireciono para outra página
     this.router.navigate(["/home"]);
   }
 
   // verificar se usuario já fez autenticação
+  //(autorização do acesso)
   estaAutenticado(): boolean{
+    //transformando a verificação da String em Booleana
     return !! localStorage.getItem(this.CHAVE_AUTH);
   }
 
   //pegar os dados do usuário
   getUsuarioAtual(): any{
+    // quando eu armazeno no localStorage -> Texto -
+    // quando vou pegar as informaç~eos do LocalStorage eu converto para JSON
     return JSON.parse(localStorage.getItem(this.CHAVE_AUTH) || '{}');
   }
 
